e32e7b023f8e860209cfe7788cef18fbd5847b2f
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2024-05-30 09:05:22 -0500

    feat: :bug: validation implementation
    \

--- src/app/productos/add-producto/add-producto.page.ts
+++ src/app/productos/add-producto/add-producto.page.ts
@@ -1,47 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IProducto } from 'src/app/interfaces/iproducto';
-import { IProductos } from 'src/app/interfaces/iproductos';
 import { SProductosService } from 'src/app/services/sproductos.service';
+import { IProducto } from 'src/app/interfaces/iproducto';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-producto',
   templateUrl: './add-producto.page.html',
   styleUrls: ['./add-producto.page.scss'],
 })
-
 export class AddProductoPage implements OnInit {
-
-
-
-  newProducto:IProducto ={
- 
-    presupuesto:"",
-    unidad:"",
-    producto:"",
-    cantidad:0,
-    valorUnitario:0,
-    valorTotal:0,
-    fecha:"",
-    proveedor:""
-  }
+  newProducto: IProducto = {
+    presupuesto: '',
+    unidad: '',
+    producto: '',
+    cantidad: 0,
+    valorUnitario: 0,
+    valorTotal: 0,
+    fecha: '',
+    proveedor: ''
+  };
 
   constructor(
-    private productoService:SProductosService,
-    private router:Router
+    private productoService: SProductosService,
+    private router: Router,
+    private alertController: AlertController
   ) { }
 
-  ngOnInit() {
-  }
-  
+  ngOnInit() { }
+
   calcularValorTotal() {
     this.newProducto.valorTotal = this.newProducto.cantidad * this.newProducto.valorUnitario;
   }
 
-  crearProducto(){
-    this.productoService.crearProductos(this.newProducto).subscribe()
-    this.router.navigateByUrl( 'list-productos' )
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: message,
+      buttons: ['OK']
+    });
 
+    await alert.present();
   }
-  
+
+  onSubmit(form: { valid: any; }) {
+    if (form.valid) {
+      this.calcularValorTotal();
+      this.productoService.crearProductos(this.newProducto).subscribe(
+        response => {
+          console.log('Producto guardado:', response);
+          this.router.navigateByUrl('list-productos');
+        },
+        error => {
+          this.presentAlert('Error al guardar el producto. Inténtalo nuevamente.');
+        }
+      );
+    } else {
+      this.presentAlert('Por favor completar todos los campos.');
+    }
+  }
 }
