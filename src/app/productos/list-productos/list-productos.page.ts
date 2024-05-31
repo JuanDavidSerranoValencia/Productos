ff24d953396c8d7ad74c44da33fba49c23c46e66
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2024-05-31 12:04:42 -0500

    feat: :art: interface and utility improvements

--- src/app/productos/list-productos/list-productos.page.ts
+++ src/app/productos/list-productos/list-productos.page.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
 import { IProductos } from 'src/app/interfaces/iproductos';
 import { SProductosService } from 'src/app/services/sproductos.service';
@@ -11,6 +11,11 @@ import { SProductosService } from 'src/app/services/sproductos.service';
 export class ListProductosPage {
 
   productos: IProductos[] = [];
+  filteredProductos: IProductos[] = [];
+  filtro = {
+    unidad: '',
+    producto: ''
+  };
 
   constructor(private productosServ: SProductosService, private loadingCtrl: LoadingController) { }
 
@@ -19,30 +24,42 @@ export class ListProductosPage {
   }
 
   async loadProductos(event?: InfiniteScrollCustomEvent) {
-
     const loading = await this.loadingCtrl.create({
       message: "Cargando productos...",
       spinner: "bubbles"
-
-    }
-    );
+    });
     await loading.present();
 
     this.productosServ.ListarProductos().subscribe(
-      (resp: IProductos) => {
+      (resp: IProductos[]) => {
         loading.dismiss();
-        let listString = JSON.stringify(resp)
-        this.productos = JSON.parse(listString)
+        this.productos = resp;
+        this.filteredProductos = resp; // Inicialmente, mostrar todos los productos
         event?.target.complete();
       },
       (err) => {
-        console.log(err.message())
+        console.log(err.message);
         loading.dismiss();
       }
-    )
-
-
+    );
   }
 
+  applyFilter() {
+    this.filteredProductos = this.productos.filter(producto => {
+      return (!this.filtro.unidad || producto.unidad.toLowerCase().includes(this.filtro.unidad.toLowerCase())) &&
+             (!this.filtro.producto || producto.producto.toLowerCase().includes(this.filtro.producto.toLowerCase()));
+    });
+  }
 
+  onFilterChange() {
+    this.applyFilter();
+  }
+
+  clearFilters() {
+    this.filtro = {
+      unidad: '',
+      producto: ''
+    };
+    this.filteredProductos = this.productos;
+  }
 }
